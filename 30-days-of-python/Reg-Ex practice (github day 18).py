33a19f61980adddeac13e3ae29c46665a14a4601
Khaled Abdul <khaledabduldps@gmail.com>
2026-08-01 13:14:46 +0300

    modified reg-ex pratice file

--- 30-days-of-python/Reg-Ex practice (github day 18).py
+++ 30-days-of-python/Reg-Ex practice (github day 18).py
@@ -3,7 +3,7 @@ text = """
 Contact List & Logs:
 - Alice Smith: alice.smith@example.com, +1-555-0192, 1024 Elm St. (Zip: 90210)
 - Bob Jones: b_jones1984@work-place.org, 555.839.2019, 44 Oak Ave. (Zip: 10001)
-- Charlie: charlie@domain.co.uk, 5550183829, 89 Pine Rd. 
+- Charlie: charlie@domain.co.uk, 5550183829, 89 Pine Rd.
 
 Log entries on 2026-06-15:
 [INFO] User login from IP 192.168.1.50
@@ -21,23 +21,26 @@ Log entries on 2026-06-15:
 #     print(f"Name: {match}")
 
 
-text_2 = """
-# User Profiles and Contacts
-John Doe: john.doe@example.com | Phone: +1-555-0199 | ID: 1042
-Jane_Smith_99: jane.smith@work-place.net | Phone: 555.0142 | ID: 8831
-bob_builder@test.co.uk | Phone: (555) 012-3456 | ID: 0012
+# Level 1
+# Ex 1
 
-# Server Logs and IP Addresses
-2026-06-01 08:30:12 [INFO] Connection established from 192.168.1.50
-2026-06-01 08:31:05 [WARNING] High memory usage on 10.0.0.99
-2026-06-01 09:15:00 [ERROR] Failed login attempt from 256.300.1.1 (Invalid IP)
+# paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
 
-# Postal Codes and Prices
-Item A costs $45.99 (SKU: AB-1234)
-Item B costs $1,200.00 (SKU: XY-9876)
-Zip Codes: 90210, 10001-1234, and 30301.
 
-# Random text with repeating patterns
-banana, bandana, band, granada, cabana
-12345, 1234, 123, 12, 1
-"""
+def most_frequent_word(string):
+    pattern = re.compile(r"\b\w+\b", re.IGNORECASE)
+    all_words = pattern.findall(paragraph)
+    word_count = {}
+    for word in all_words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    sorted_words = sorted([(count, word)
+                          for word, count in word_count.items()], reverse=True)
+    return sorted_words
+
+
+print(most_frequent_word(paragraph))
+
+# Ex 2
